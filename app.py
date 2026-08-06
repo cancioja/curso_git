670798d45d0a0719a3038dc75b0bc1b8d06a5470
jcancio <cancioja@gmail.com>
2026-08-06 01:13:57 -0300

    feat: permite convertir en ambas direcciones

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 st.title("Conversor de Temperatura")
-st.write("Ingrese la temperatura en Celsius:")
-celsius = st.number_input("Celsius", value=0.0)
-fahrenheit = (celsius * 9/5) + 32
-st.write(f"{celsius} ºC son {fahrenheit} ºF")
+
+modo = st.radio("Seleccione el modo de conversión:",["Celsius a Fahrenheit", "Fahrenheit a Celsius"])
+st.write("Ingrese la temperatura:")
+temperatura = st.number_input("Temperatura", value=0.0)
+
+if modo == "Celsius a Fahrenheit":
+    resultado = (temperatura * 9/5) + 32
+    st.write(f"{temperatura} ºC son {resultado} ºF")
+else:
+    resultado = (temperatura - 32) * 5/9
+    st.write(f"{temperatura} ºF son {resultado} ºC")
