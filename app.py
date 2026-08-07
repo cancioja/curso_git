91d0b86c9b331e6e74a5ec2ff97221ed74e86164
jcancio <cancioja@gmail.com>
2026-08-07 01:03:39 -0300

    fix: redondea el resultado a 2 decimales

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ temperatura = st.number_input("Temperatura", value=0.0)
 
 if modo == "Celsius a Fahrenheit":
     resultado = (temperatura * 9/5) + 32
-    st.write(f"{temperatura} ºC son {resultado} ºF")
+    st.write(f"{temperatura} ºC son {round(resultado, 2)} ºF")
 else:
     resultado = (temperatura - 32) * 5/9
-    st.write(f"{temperatura} ºF son {resultado} ºC")
+    st.write(f"{temperatura} ºF son {round(resultado, 2)} ºC")
